cf646ab70543a055af2dbb9c753bc090b5d1019f
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-01 22:23:40 +0200

    Add scheduled extra-turn DTO test

---
+++ king-of-tokyo-engine/tests/KingOfTokyo.Core.Tests/Dto/GameStateDtoScheduledTurnsTests.cs
@@ -0,0 +1,30 @@
+using KingOfTokyo.Core.Domain.Entities;
+using KingOfTokyo.Core.Domain.State;
+using KingOfTokyo.Core.Dto;
+using Xunit;
+
+namespace KingOfTokyo.Core.Tests.Dto;
+
+public sealed class GameStateDtoScheduledTurnsTests
+{
+    [Fact]
+    public void ToDto_Should_MapScheduledTurnPlayerIds()
+    {
+        var gameState = CreateGameState(4);
+        gameState.ScheduleExtraTurn(2);
+        gameState.ScheduleExtraTurn(0);
+
+        var dto = gameState.ToDto();
+
+        Assert.Equal(new[] { 2, 0 }, dto.ScheduledTurnPlayerIds);
+    }
+
+    private static GameState CreateGameState(int playerCount)
+    {
+        var players = Enumerable.Range(0, playerCount)
+            .Select(i => new PlayerState(i, $"Monster {i + 1}"))
+            .ToArray();
+
+        return new GameState(players, new GameOptions(playerCount));
+    }
+}
