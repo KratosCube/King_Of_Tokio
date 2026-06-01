b00e4f5d139e06c16081da171f7e22c7d56ae26b
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-02 01:01:52 +0200

    Add Frenzy card id

--- king-of-tokyo-engine/src/KingOfTokyo.Core/Domain/ValueObjects/KnownCardIds.cs
+++ king-of-tokyo-engine/src/KingOfTokyo.Core/Domain/ValueObjects/KnownCardIds.cs
@@ -66,4 +66,5 @@ public static class KnownCardIds
     public const string SmokeCloud = "card-smoke-cloud";
     public const string MonsterBatteries = "card-monster-batteries";
     public const string FreezeTime = "card-freeze-time";
+    public const string Frenzy = "card-frenzy";
 }
