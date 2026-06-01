671600b64f13dde5021c5d7986216785425e4424
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-01 17:43:04 +0200

    Wire herd culler command into game engine

--- king-of-tokyo-engine/src/KingOfTokyo.Core/Engine/GameEngine.cs
+++ king-of-tokyo-engine/src/KingOfTokyo.Core/Engine/GameEngine.cs
@@ -72,6 +72,7 @@ public sealed class GameEngine : IGameEngine
                 ActivateRapidHealingCommand activateRapidHealingCommand => ExecuteActivateRapidHealing(gameState, activateRapidHealingCommand),
                 ActivateTelepathCommand activateTelepathCommand => ExecuteActivateTelepath(gameState, activateTelepathCommand),
                 ActivateStretchyCommand activateStretchyCommand => ExecuteActivateStretchy(gameState, activateStretchyCommand),
+                ActivateHerdCullerCommand activateHerdCullerCommand => ExecuteActivateHerdCuller(gameState, activateHerdCullerCommand),
                 PeekTopDeckCardCommand peekTopDeckCardCommand => ExecutePeekTopDeckCard(gameState, peekTopDeckCardCommand),
                 BuyPeekedTopDeckCardCommand buyPeekedTopDeckCardCommand => ExecuteBuyPeekedTopDeckCard(gameState, buyPeekedTopDeckCardCommand),
                 DeclinePeekedTopDeckCardCommand declinePeekedTopDeckCardCommand => ExecuteDeclinePeekedTopDeckCard(gameState, declinePeekedTopDeckCardCommand),
@@ -89,195 +90,137 @@ public sealed class GameEngine : IGameEngine
     private CommandResult ExecuteInitialize(GameState gameState, InitializeGameCommand command)
     {
         _validator.EnsureCanInitializeGame(gameState);
-
         gameState.StartGame();
         _marketSetupService.InitializeMarket(gameState);
-
         return CommandResult.Successful(gameState);
     }
 
     private CommandResult ExecuteBeginTurn(GameState gameState, BeginTurnCommand command)
     {
         _validator.EnsureCanBeginTurn(gameState, command);
-
         var currentPlayer = gameState.GetCurrentPlayer();
         var diceCount = _keepCardRulesService.GetEffectiveDiceCount(currentPlayer);
         var maxRolls = 3 + _keepCardRulesService.GetExtraRerolls(currentPlayer);
-
         gameState.StartTurnForCurrentPlayer(diceCount: diceCount, maxRolls: maxRolls);
-
         currentPlayer = gameState.GetCurrentPlayer();
-        var newEvents = new List<GameEventBase>
-        {
-            new TurnStartedEvent(currentPlayer.PlayerId)
-        };
+        var newEvents = new List<GameEventBase> { new TurnStartedEvent(currentPlayer.PlayerId) };
 
         if (gameState.CurrentTurn!.Flags.StartedTurnInTokyo)
         {
             currentPlayer.GainVictoryPoints(2);
             gameState.CurrentTurn.Flags.ScoredVictoryPoints = true;
-
-            newEvents.Add(new VictoryPointsGainedEvent(
-                currentPlayer.PlayerId,
-                2,
-                "Started turn in Tokyo."));
+            newEvents.Add(new VictoryPointsGainedEvent(currentPlayer.PlayerId, 2, "Started turn in Tokyo."));
 
             var urbavoreBonus = _keepCardRulesService.GetBonusStartTurnTokyoVictoryPoints(currentPlayer);
             if (urbavoreBonus > 0)
             {
                 currentPlayer.GainVictoryPoints(urbavoreBonus);
-                newEvents.Add(new VictoryPointsGainedEvent(
-                    currentPlayer.PlayerId,
-                    urbavoreBonus,
-                    "Keep card: Urbavore."));
+                newEvents.Add(new VictoryPointsGainedEvent(currentPlayer.PlayerId, urbavoreBonus, "Keep card: Urbavore."));
             }
         }
 
         gameState.CurrentTurn.SetPhase(TurnPhase.Rolling);
         gameState.ClearPendingDecision();
-
         PublishEvents(newEvents);
-
         return CommandResult.Successful(gameState, newEvents);
     }
 
     private CommandResult ExecuteRollDice(GameState gameState, RollDiceCommand command)
     {
         _validator.EnsureCanRollDice(gameState, command);
-
         var currentTurn = gameState.CurrentTurn!;
         var currentPlayer = gameState.GetCurrentPlayer();
-
         _diceRollService.RollAll(currentTurn.DicePool);
         currentTurn.IncrementRollCount();
-
-        var newEvents = new List<GameEventBase>
-        {
-            new DiceRolledEvent(
-                currentPlayer.PlayerId,
-                currentTurn.RollCountUsed,
-                currentTurn.DicePool.Dice.Select(d => d.CurrentFace).ToArray())
-        };
-
+        var newEvents = new List<GameEventBase> { new DiceRolledEvent(currentPlayer.PlayerId, currentTurn.RollCountUsed, currentTurn.DicePool.Dice.Select(d => d.CurrentFace).ToArray()) };
         var pendingDecision = CreateRerollDecisionIfAvailable(currentTurn);
         gameState.SetPendingDecision(pendingDecision);
-
         PublishEvents(newEvents);
-
         return CommandResult.Successful(gameState, newEvents, pendingDecision);
     }
 
     private CommandResult ExecuteRerollDice(GameState gameState, RerollDiceCommand command)
     {
         _validator.EnsureCanRerollDice(gameState, command);
-
         var currentTurn = gameState.CurrentTurn!;
         var currentPlayer = gameState.GetCurrentPlayer();
-
         _diceRollService.RerollSelected(currentTurn.DicePool, command.DiceIndexesToReroll);
         currentTurn.IncrementRollCount();
-
-        var newEvents = new List<GameEventBase>
-        {
-            new DiceRolledEvent(
-                currentPlayer.PlayerId,
-                currentTurn.RollCountUsed,
-                currentTurn.DicePool.Dice.Select(d => d.CurrentFace).ToArray())
-        };
-
+        var newEvents = new List<GameEventBase> { new DiceRolledEvent(currentPlayer.PlayerId, currentTurn.RollCountUsed, currentTurn.DicePool.Dice.Select(d => d.CurrentFace).ToArray()) };
         var pendingDecision = CreateRerollDecisionIfAvailable(currentTurn);
         gameState.SetPendingDecision(pendingDecision);
-
         PublishEvents(newEvents);
-
         return CommandResult.Successful(gameState, newEvents, pendingDecision);
     }
 
     private CommandResult ExecuteFinalizeDice(GameState gameState, FinalizeDiceCommand command)
     {
         _validator.EnsureCanFinalizeDice(gameState, command);
-
         var stepResult = _finalizeDiceService.Execute(gameState);
-
         PublishEvents(stepResult.Events);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteChooseLeaveTokyo(GameState gameState, ChooseLeaveTokyoCommand command)
     {
         _validator.EnsureCanChooseLeaveTokyo(gameState, command);
-
         var stepResult = _tokyoDecisionService.Execute(gameState, command.LeaveTokyo);
-
         PublishEvents(stepResult.Events);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteBuyFaceUpCard(GameState gameState, BuyFaceUpCardCommand command)
     {
-        var card = gameState.Market.FaceUpCards[command.SlotIndex]
-            ?? throw new InvalidOperationException("Selected market slot is empty.");
-
+        var card = gameState.Market.FaceUpCards[command.SlotIndex] ?? throw new InvalidOperationException("Selected market slot is empty.");
         var currentPlayer = gameState.GetCurrentPlayer();
         var effectiveCost = _keepCardRulesService.GetEffectivePurchaseCost(currentPlayer, card);
-
         _validator.EnsureCanBuyFaceUpCard(gameState, command, effectiveCost);
-
         var stepResult = _marketPurchaseService.BuyFaceUpCard(gameState, command.SlotIndex, effectiveCost);
-
         PublishEvents(stepResult.Events);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteRefreshMarket(GameState gameState, RefreshMarketCommand command)
     {
         _validator.EnsureCanRefreshMarket(gameState, command);
-
         var stepResult = _marketRefreshService.Refresh(gameState);
-
         PublishEvents(stepResult.Events);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteActivateRapidHealing(GameState gameState, ActivateRapidHealingCommand command)
     {
         _validator.EnsureCanActivateRapidHealing(gameState, command);
-
         var stepResult = _rapidHealingService.Activate(gameState);
-
         PublishEvents(stepResult.Events);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteActivateTelepath(GameState gameState, ActivateTelepathCommand command)
     {
         _validator.EnsureCanActivateTelepath(gameState, command);
-
         var stepResult = _specialCardActivationService.ActivateTelepath(gameState);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteActivateStretchy(GameState gameState, ActivateStretchyCommand command)
     {
         _validator.EnsureCanActivateStretchy(gameState, command);
-
         var stepResult = _specialCardActivationService.ActivateStretchy(gameState, command.DieIndex, command.TargetFace);
+        return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
+    }
 
+    private CommandResult ExecuteActivateHerdCuller(GameState gameState, ActivateHerdCullerCommand command)
+    {
+        _validator.EnsureCanActivateHerdCuller(gameState, command);
+        var stepResult = _specialCardActivationService.ActivateHerdCuller(gameState, command.DieIndex);
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecutePeekTopDeckCard(GameState gameState, PeekTopDeckCardCommand command)
     {
         _validator.EnsureCanPeekTopDeckCard(gameState, command);
-
         var stepResult = _specialCardActivationService.PeekTopDeckCard(gameState);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
@@ -286,43 +229,32 @@ public sealed class GameEngine : IGameEngine
         var topCard = gameState.Market.PeekTopDrawCard();
         var currentPlayer = gameState.GetCurrentPlayer();
         var effectiveCost = _keepCardRulesService.GetEffectivePurchaseCost(currentPlayer, topCard);
-
         _validator.EnsureCanBuyPeekedTopDeckCard(gameState, command, effectiveCost);
-
         var stepResult = _marketPurchaseService.BuyTopDeckCard(gameState, effectiveCost);
         gameState.ClearPendingDecision();
-
         PublishEvents(stepResult.Events);
-
         return CommandResult.Successful(gameState, stepResult.Events, null);
     }
 
     private CommandResult ExecuteDeclinePeekedTopDeckCard(GameState gameState, DeclinePeekedTopDeckCardCommand command)
     {
         _validator.EnsureCanDeclinePeekedTopDeckCard(gameState, command);
-
         var stepResult = _specialCardActivationService.DeclinePeekedTopDeckCard(gameState);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteEndTurn(GameState gameState, EndTurnCommand command)
     {
         _validator.EnsureCanEndTurn(gameState, command);
-
         var stepResult = _turnLifecycleService.EndTurn(gameState);
-
         PublishEvents(stepResult.Events);
-
         return CommandResult.Successful(gameState, stepResult.Events, stepResult.PendingDecision);
     }
 
     private CommandResult ExecuteAdvanceToNextPlayer(GameState gameState, AdvanceToNextPlayerCommand command)
     {
         _validator.EnsureCanAdvanceToNextPlayer(gameState, command);
-
         _turnLifecycleService.AdvanceToNextPlayer(gameState);
-
         return CommandResult.Successful(gameState);
     }
 
@@ -339,13 +271,8 @@ public sealed class GameEngine : IGameEngine
             PlayerId = currentTurn.CurrentPlayerId,
             Payload = new RerollDecisionData
             {
-                CurrentLockedDiceIndexes = currentTurn.DicePool.Dice
-                    .Where(d => d.IsLocked)
-                    .Select(d => d.Index)
-                    .ToArray(),
-                CurrentFaces = currentTurn.DicePool.Dice
-                    .Select(d => d.CurrentFace)
-                    .ToArray(),
+                CurrentLockedDiceIndexes = currentTurn.DicePool.Dice.Where(d => d.IsLocked).Select(d => d.Index).ToArray(),
+                CurrentFaces = currentTurn.DicePool.Dice.Select(d => d.CurrentFace).ToArray(),
                 RollCountUsed = currentTurn.RollCountUsed,
                 MaxRolls = currentTurn.MaxRolls
             }
