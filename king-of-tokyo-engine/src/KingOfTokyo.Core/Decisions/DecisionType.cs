b4c4b5d9ab84fa88830e40605826e059ee08bb47
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-02 12:59:20 +0200

    Add Opportunist decision type

--- king-of-tokyo-engine/src/KingOfTokyo.Core/Decisions/DecisionType.cs
+++ king-of-tokyo-engine/src/KingOfTokyo.Core/Decisions/DecisionType.cs
@@ -5,5 +5,6 @@ public enum DecisionType
     None = 0,
     SelectDiceToReroll = 1,
     LeaveTokyo = 2,
-    PeekTopDeckCardPurchase = 3
+    PeekTopDeckCardPurchase = 3,
+    OpportunistPurchase = 4
 }
