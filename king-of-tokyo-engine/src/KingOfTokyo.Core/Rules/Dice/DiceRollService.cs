b060a58ea09b7254e79a5cb59b3c461c844484d1
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-02 12:01:12 +0200

    Apply Background Dweller automatic rerolls

--- king-of-tokyo-engine/src/KingOfTokyo.Core/Rules/Dice/DiceRollService.cs
+++ king-of-tokyo-engine/src/KingOfTokyo.Core/Rules/Dice/DiceRollService.cs
@@ -1,5 +1,7 @@
 using KingOfTokyo.Core.Abstractions;
 using KingOfTokyo.Core.Domain.Entities;
+using KingOfTokyo.Core.Domain.Enums;
+using KingOfTokyo.Core.Domain.ValueObjects;
 
 namespace KingOfTokyo.Core.Rules.Dice;
 
@@ -14,15 +16,27 @@ public sealed class DiceRollService
 
     public void RollAll(DicePoolState dicePool)
     {
+        RollAll(dicePool, player: null);
+    }
+
+    public void RollAll(DicePoolState dicePool, PlayerState? player)
+    {
         ArgumentNullException.ThrowIfNull(dicePool);
 
         foreach (var die in dicePool.Dice)
         {
             die.SetFace(_randomSource.RollDieFace());
         }
+
+        RerollBackgroundDwellerThreesIfNeeded(dicePool, player);
     }
 
     public void RerollSelected(DicePoolState dicePool, IReadOnlyCollection<int> diceIndexesToReroll)
+    {
+        RerollSelected(dicePool, diceIndexesToReroll, player: null);
+    }
+
+    public void RerollSelected(DicePoolState dicePool, IReadOnlyCollection<int> diceIndexesToReroll, PlayerState? player)
     {
         ArgumentNullException.ThrowIfNull(dicePool);
         ArgumentNullException.ThrowIfNull(diceIndexesToReroll);
@@ -43,6 +57,34 @@ public sealed class DiceRollService
         foreach (var index in diceIndexesToReroll)
         {
             dicePool.SetFace(index, _randomSource.RollDieFace());
+        }
+
+        RerollBackgroundDwellerThreesIfNeeded(dicePool, player);
+    }
+
+    private void RerollBackgroundDwellerThreesIfNeeded(DicePoolState dicePool, PlayerState? player)
+    {
+        if (player is null || !player.HasKeepCard(KnownCardIds.BackgroundDweller))
+        {
+            return;
+        }
+
+        while (true)
+        {
+            var threeIndexes = dicePool.Dice
+                .Where(die => die.CurrentFace == DieFace.Three)
+                .Select(die => die.Index)
+                .ToArray();
+
+            if (threeIndexes.Length == 0)
+            {
+                return;
+            }
+
+            foreach (var index in threeIndexes)
+            {
+                dicePool.SetFace(index, _randomSource.RollDieFace());
+            }
         }
     }
 }
