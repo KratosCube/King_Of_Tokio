d5139babbe70d00f6bd616ec9526b7609d291069
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-05 01:42:32 +0200

    Register lobby store and endpoints

--- king-of-tokyo-engine/src/KingOfTokyo.Api/Program.cs
+++ king-of-tokyo-engine/src/KingOfTokyo.Api/Program.cs
@@ -1,12 +1,15 @@
 using KingOfTokyo.Api.Endpoints;
 using KingOfTokyo.Api.GameSessions;
+using KingOfTokyo.Api.Lobbies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IGameSessionStore, InMemoryGameSessionStore>();
+builder.Services.AddSingleton<ILobbyStore, InMemoryLobbyStore>();
 
 var app = builder.Build();
 
 app.MapKingOfTokyoGameEndpoints();
+app.MapKingOfTokyoLobbyEndpoints();
 
 app.Run();
