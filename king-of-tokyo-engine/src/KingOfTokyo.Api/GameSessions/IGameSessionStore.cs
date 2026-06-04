cb231bd6029bd35f7dd2c570d06e1a3d9c52291c
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-04 22:44:04 +0200

    Add game session store interface

---
+++ king-of-tokyo-engine/src/KingOfTokyo.Api/GameSessions/IGameSessionStore.cs
@@ -0,0 +1,17 @@
+using KingOfTokyo.Api.Contracts;
+using KingOfTokyo.Core.Domain.State;
+using KingOfTokyo.Core.Dto;
+using KingOfTokyo.Core.Engine;
+
+namespace KingOfTokyo.Api.GameSessions;
+
+public interface IGameSessionStore
+{
+    GameStateDto CreateGame(IReadOnlyList<string> monsterNames);
+
+    bool TryGetSnapshot(Guid gameId, out GameStateDto? snapshot);
+
+    bool TryGetEvents(Guid gameId, long fromEventSequenceExclusive, out GameEventCursorDto? cursor);
+
+    bool TryExecute(Guid gameId, Func<GameEngine, GameState, CommandResult> execute, out ApiCommandResultDto? result);
+}
