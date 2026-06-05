2479314fd3240233341d6309a0b5c1a2d6149432
Václav Chuchlík <106406265+KratosCube@users.noreply.github.com>
2026-06-05 03:45:57 +0200

    Allow local Blazor development origins

--- king-of-tokyo-engine/src/KingOfTokyo.Api/Program.cs
+++ king-of-tokyo-engine/src/KingOfTokyo.Api/Program.cs
@@ -4,10 +4,27 @@ using KingOfTokyo.Api.Lobbies;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LocalBlazorDevelopment", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://localhost:5173",
+                "http://127.0.0.1:5173",
+                "https://127.0.0.1:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddSingleton<IGameSessionStore, InMemoryGameSessionStore>();
 builder.Services.AddSingleton<ILobbyStore, InMemoryLobbyStore>();
 
 var app = builder.Build();
+
+app.UseCors("LocalBlazorDevelopment");
 
 app.MapKingOfTokyoGameEndpoints();
 app.MapKingOfTokyoLobbyEndpoints();
